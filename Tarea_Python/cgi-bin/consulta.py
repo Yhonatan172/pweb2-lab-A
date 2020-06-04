15ddc16c87cb64072c40e6676f07457843e5ed23
Yhonatan <yylaccana@unsa.edu.pe>
2020-06-03 22:50:10 -0500

    he modificado la ruta del archivo de información

--- Tarea_Python/cgi-bin/consulta.py
+++ Tarea_Python/cgi-bin/consulta.py
@@ -2,18 +2,20 @@
 import re
 import json
 def matchLine(line):
- dic = {}
- x = re.search("^\[([0-9]+)\] ((.+), )?(.+); ((.+)|(http.+));(.+)\.", line)
- dic["number"] = x.group(1)
- dic["author"] = x.group(3)
- dic["title"] = x.group(4)
- dic["editor"] = x.group(5)
- dic["year"] = x.group(8)
- return dic
-f = open("bibliografia.txt")
+	 dic = {}
+	 x = re.search("^\[([0-9]+)\] ((.+), )?(.+); ((.+)|(http.+));(.+)\.", line)
+	 dic["number"] = x.group(1)
+	 dic["author"] = x.group(3)
+	 dic["title"] = x.group(4)
+	 dic["editor"] = x.group(5)
+	 dic["year"] = x.group(8)
+ 	return dic
+f = open("Base_De_Datos.txt")
 data = []
 for line in f:
  dic = matchLine(line)
  data.append(dic)
 print("Content-Type: application/json\n\n")
 print(json.dumps(data))
+
+
