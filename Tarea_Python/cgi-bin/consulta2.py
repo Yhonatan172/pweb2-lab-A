f8bc675f469308854c66a4c266bc8ab2bdd1cb9c
Yhonatan <yylaccana@unsa.edu.pe>
2020-06-08 10:18:27 -0500

    No puedo como pasar los datos de Kind y Keyword a consulta2.py de consulta.js

--- Tarea_Python/cgi-bin/consulta2.py
+++ Tarea_Python/cgi-bin/consulta2.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3	
 import sqlite3
+import json
+import cgi
+@app.route('/getmethod/<jsdata>')
+def get_javascript_data(jsdata):
+       return jsdata
+$.get( "/getmethod/<javascript_data>" );
 conn = sqlite3.connect('imdb.db')
+c = conn.cursor()
+tabla=
+c.execute('SELECT * FROM actor')	
