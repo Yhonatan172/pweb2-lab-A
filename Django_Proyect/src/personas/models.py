ea03fb81752d1ec5b65877c1a53f6185a2fa955f
YHONATAN <yylaccana@unsa.edu.pe>
2020-06-13 23:32:20 -0500

    Correcion del nombre de la clase 'Persona'

--- Django_Proyect/src/personas/models.py
+++ Django_Proyect/src/personas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Personas(models.Model):
+class Persona(models.Model):
     nombres   = models.TextField()
     apellidos = models.TextField()
     edad      = models.TextField()
